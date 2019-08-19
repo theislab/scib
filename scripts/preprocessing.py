88f1b3e42b2f942b59373bfd8406db4ebde1d022
Michaela Mueller <michaela.mueller@helmholtz-muenchen.de>
2019-08-19 19:16:26 +0200

    using interscection HVG method + little fix in preprocessing script

--- scripts/preprocessing.py
+++ scripts/preprocessing.py
@@ -5,7 +5,7 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Normalisation with scran')
 parser.add_argument('-i', '--input_file', required=True)
-parser.add_argument('-c', '--cc_markers') # TODO: split in s genes and g2m genes
+parser.add_argument('-c', '--cc_markers', help="file containing cell cycle marker genes (assumes that this is Tirosh et al. marker gene file from the scanpy tutorial https://raw.githubusercontent.com/theislab/scanpy_usage/master/180209_cell_cycle/data/regev_lab_cell_cycle_genes.txt)") # TODO: split in s genes and g2m genes
 parser.add_argument('-o', '--output_file', required=True)
 args = parser.parse_args()
 
@@ -23,7 +23,7 @@ pp.normalize(adata)
 # Cell cycle scores
 if args.cc_markers is not None:
     print("Cell cycle scores")
-    s_genes, g2m_genes = pp.cc_tirosh(cc_markers)
+    s_genes, g2m_genes = pp.cc_tirosh(args.cc_markers)
     sc.tl.score_genes_cell_cycle(adata, s_genes=s_genes, g2m_genes=g2m_genes)
 
 adata.write(args.output_file)
