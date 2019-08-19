f2800f06337820780a02f9c484f50ec4e71687dd
Michaela Mueller <michaela.mueller@helmholtz-muenchen.de>
2019-08-19 17:32:27 +0200

    fixed nan removal in 10X

--- scripts/preprocessing.py
+++ scripts/preprocessing.py
@@ -7,9 +7,6 @@ parser = argparse.ArgumentParser(description='Normalisation with scran')
 parser.add_argument('-i', '--input_file', required=True)
 parser.add_argument('-c', '--cc_markers') # TODO: split in s genes and g2m genes
 parser.add_argument('-o', '--output_file', required=True)
-parser.add_argument('--flavor', default='cell_ranger', help='For HVG computation. Either "cell_ranger" (for log-transformed data) or "seurat" (for no-transformed data).')
-parser.add_argument('--n_top_genes', default=4000, help='Number of HVG to be computed.')
-parser.add_argument('-v', '--visualisation', action='store_true')
 args = parser.parse_args()
 
 import pandas as pd
@@ -22,12 +19,6 @@ adata = sc.read(args.input_file, cache=True)
 # Normalisation
 print("Normalisation")
 pp.normalize(adata)
-
-# Visualisation
-if args.visualisation:
-    print("Visualisation")
-    pp.reduce_data(adata, hvg=True, flavor=args.flavor, n_top_genes=args.n_top_genes,
-                   pca=True, neighbors=True, umap=True)
 
 # Cell cycle scores
 if args.cc_markers is not None:
