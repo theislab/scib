b0d0fc4b97bd265d70f698d88a9334b40e57d015
Michaela Müller <mi.mueller@tum.de>
2019-09-04 19:38:48 +0200

    scgen fix refined

--- scripts/runIntegration.py
+++ scripts/runIntegration.py
@@ -6,14 +6,13 @@ import scIB
 import warnings
 warnings.filterwarnings('ignore')
 
-def runIntegration(inPath, outPath, method, hvg, batch, max_genes_hvg, groups=None):
+def runIntegration(inPath, outPath, method, hvg, batch, max_genes_hvg, group=None):
     """
     params:
         method: name of method
-        hvg: number of highly variable genes
         batch: name of `adata.obs` column of the batch
-        max_genes_hvg: 
-        groups: only needed 
+        max_genes_hvg: maximum number of HVG
+        groups: name of cell type or cluster column. Only needed for scGen (atm)
     """
 
     adata = sc.read(inPath)
@@ -21,7 +20,7 @@ def runIntegration(inPath, outPath, method, hvg, batch, max_genes_hvg, groups=No
     if hvg > 500:
         adata = scIB.preprocessing.hvg_intersect(adata, batch, adataOut=True, target_genes=hvg, n_stop=max_genes_hvg)
     
-    if method == "scgen":
+    if group is not None:
         integrated_tmp = scIB.metrics.measureTM(method, adata, batch, group)
     else:
         integrated_tmp = scIB.metrics.measureTM(method, adata, batch)
@@ -43,6 +42,7 @@ if __name__=='__main__':
     parser.add_argument('-i', '--input_file', required=True)
     parser.add_argument('-o', '--output_file', required=True)
     parser.add_argument('-b', '--batch', required=True, help='Batch variable')
+    parser.add_argument('-c', '--group', default=None, help='Group variable (cell type or cluster)')
     parser.add_argument('-v', '--hvgs', help='Number of highly variable genes', default=2000)
     parser.add_argument('-g', '--max_genes_hvg', help='Maximum number of HVGs per dataset to get the intersect value specified by --hvgs', default=8000)
 
