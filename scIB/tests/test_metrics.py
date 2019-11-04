fb9f61712f89fac97117a3f3f465f30f7adc8cd9
Michaela Mueller <michaela.mueller@helmholtz-muenchen.de>
2019-11-04 19:31:16 +0100

    removed HVG selection for PC regression and adapted tests accordingly

--- scIB/tests/test_metrics.py
+++ scIB/tests/test_metrics.py
@@ -16,7 +16,7 @@ def cluster(adata, label_key, cluster_key, verbose=False):
 def silhouette():
     adata = utils.create_adata_dummy(pca=True, n_top_genes=2000)
     score = me.silhouette(adata, group_key='celltype', embed='X_pca', scale=True)
-    print(score)
+    print(f"score: {score}")
     assert score >= 0
     assert score <= 1
     
@@ -25,7 +25,7 @@ def silhouette_batch():
     _, sil = me.silhouette_batch(adata, batch_key='batch', group_key='celltype', 
                                       embed='X_pca', scale=True, verbose=False)
     score = sil['silhouette_score'].mean()
-    print(score)
+    print(f"score: {score}")
     assert score >= 0
     assert score <= 1
     
@@ -40,32 +40,36 @@ def ari():
     adata = utils.create_adata_dummy(pca=True, n_top_genes=2000, neighbors=True)
     cluster(adata, cluster_key='cluster', label_key='celltype')
     score = me.ari(adata, group1='cluster', group2='celltype')
-    print(score)
+    print(f"score: {score}")
     assert score >= 0
     assert score <= 1
 
 def pcr_comparison():
+    verbose = True
     # no PCA precomputed
     adata = utils.create_adata_dummy()
     adata_int = adata.copy()
-    score = me.pcr_comparison(adata, adata_int, covariate='batch', scale=True, verbose=False)
-    print(score)
+    score = me.pcr_comparison(adata, adata_int, covariate='batch', n_comps=50,
+                              scale=True, verbose=verbose)
+    print(f"no PCA precomputed\nscore: {score}")
+    assert score >= 0
+    assert score <= 1
+    
+    # use different embedding
+    adata = utils.create_adata_dummy()
+    adata_int = adata.copy()
+    utils.add_emb(adata_int, type_='full')
+    score = me.pcr_comparison(adata, adata_int, covariate='batch', embed='X_emb', n_comps=50,
+                                   scale=True, verbose=verbose)
+    print(f"using embedding\nscore: {score}")
     assert score >= 0
     assert score <= 1
     
     # precomputed PCA
     adata = utils.create_adata_dummy(pca=True, n_top_genes=2000)
     adata_int = adata.copy()
-    score = me.pcr_comparison(adata, adata_int, covariate='batch', scale=True, verbose=False)
-    print(score)
-    assert score >= 0
-    assert score <= 1
-    
-    # use different embedding
-    utils.add_emb(adata_int)
-    score = me.pcr_comparison(adata, adata_int, covariate='batch', embed='X_emb',
-                                   scale=True, verbose=False)
-    print(score)
+    score = me.pcr_comparison(adata, adata_int, covariate='batch', scale=True, verbose=verbose)
+    print(f"precomputed PCA\nscore: {score}")
     assert score >= 0
     assert score <= 1
 
@@ -74,7 +78,7 @@ def cell_cycle():
     adata_int = adata.copy()
     
     score = me.cell_cycle(adata, adata_int, batch_key='batch', organism='mouse')
-    print(score)
+    print(f"score: {score}")
     assert score >= 0
     assert score <= 1
 
@@ -82,7 +86,7 @@ def hvg_overlap():
     adata = utils.create_adata_dummy()
     adata_int = adata.copy()
     score = me.hvg_overlap(adata_int, adata, batch='batch', n_hvg=500)
-    print(score)
+    print(f"score: {score}")
     assert score >= 0
     assert score <= 1
     
