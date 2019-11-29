d1c15be9188746d20b3a653f39389de3894ad279
mbuttner <maren.buettner@helmholtz-muenchen.de>
2019-11-29 13:00:27 +0100

    bbknn larger neighborhoods for large datasets

--- scIB/integration.py
+++ scIB/integration.py
@@ -213,7 +213,10 @@ def runBBKNN(adata, batch, hvg=None):
     import bbknn
     checkSanity(adata, batch, hvg)
     sc.pp.pca(adata, svd_solver='arpack')
-    corrected = bbknn.bbknn(adata, batch_key=batch, copy=True)
+    if adata.n_obs <1e5:
+        corrected = bbknn.bbknn(adata, batch_key=batch,trim=20, neighbors_within_batch=5, copy=True)
+    if adata.n_obs >=1e5:
+        corrected = bbknn.bbknn(adata, batch_key=batch,trim=30, neighbors_within_batch=10, copy=True)
     return corrected
 
 def runConos(adata, batch, hvg=None):
