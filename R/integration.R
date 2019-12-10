1796736ab89e1f367d017d93882ec20a37677086
danielStrobl <daniel.strobl@helmholtz-muenchen.de>
2019-12-11 00:56:39 +0100

    separate R functions for R integration methods

---
+++ R/integration.R
@@ -0,0 +1,91 @@
+loadSeuratObject = function(filename) {
+	require(Seurat)
+	sobj = readRDS(filename)
+	return(sobj)
+}
+​
+runSeurat = function(data, batch, hvgs=NULL) {
+	  require(Seurat)
+​
+	  batch_list = SplitObject(data, split.by = batch)
+​
+	  anchors = FindIntegrationAnchors(
+	          object.list = batch_list,
+	          anchor.features = 2000,
+ 		  scale = T,
+		  l2.norm = T,
+		  dims = 1:30,
+        	  k.anchor = 5,
+        	  k.filter = 200,
+        	  k.score = 30,
+        	  max.features = 200,
+        	  eps = 0)
+​
+	integrated = IntegrateData(
+        	   anchorset = anchors,
+		   new.assay.name = "integrated",
+        	   features = NULL,
+        	   features.to.integrate = NULL,
+        	   dims = 1:30,
+        	   k.weight = 100,
+        	   weight.reduction = NULL,
+        	   sd.weight = 1,
+        	   sample.tree = NULL,
+        	   preserve.order = F,
+        	   do.cpp = T,
+        	   eps = 0,
+        	   verbose = T)
+​
+	return(integrated)
+}
+​
+​
+func_profiler = function(expr, chunksize=20000, filename='timing.out') {
+	      Rprof(filename, memory.profiling=T)
+	      res = expr
+	      Rprof(NULL)
+	      t = summaryRprof(filename, chunksize=chunksize, memory="both")$sampling.time
+	      mem = max(summaryRprof(filename, chunksize=chunksize, memory="both")$by.total$mem.total)
+	      return(list(results=res, time=t, memory=mem))
+}
+​
+​
+# Example call:
+#   sobj = load_seurat_object('small_test.RDS')
+#   out = func_profiler(runSeurat(sobj, "batch"))
+#   out$results is results
+#   out$time is timing
+#   out$memory is memory use
+
+
+
+
+preP <- function(so, vars.to.regress=NULL, verbose=TRUE, n.pcs=100) {
+    if (verbose) {
+    message("Running Seurat v3 workflow")
+  }
+  so <- Seurat::FindVariableFeatures(object = so, verbose = verbose)
+  so <- Seurat::ScaleData(object = so, verbose = verbose)
+  so <- Seurat::RunPCA(object = so, npcs = n.pcs, verbose = verbose)
+  return(so)
+}
+
+runConos = function(data, batch, outdir) {
+	require(conos)
+	require(Seurat)
+	sobj <- loadSeuratObject(data)
+	batch_list <- SplitObject(sobj, split.by=batch)
+ 	pp <- lapply(batch_list, preP)
+ 
+	con <- Conos$new(pp, n.cores=1)
+	con$buildGraph(space="genes")
+	con$findCommunities()
+	con$embedGraph(method="UMAP")
+	
+	#metadata <- data.frame(Cluster=con$clusters$leiden$groups)
+	dir.create(outdir)
+	
+	saveConosForScanPy(con, output.path=outdir, 
+                   norm=TRUE, embed=TRUE, pseudo.pca=TRUE, pca=TRUE, 
+                   connect=TRUE, verbose=TRUE)
+}
