5036b63ae543ad4ce90d1fe0bb104f2099e85ee2
Michaela Mueller <michaela.mueller@helmholtz-muenchen.de>
2019-12-14 18:23:08 +0100

    metrics and cell cycle working on pancreas data

---
+++ scripts/merge_cc_variance.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import pandas as pd
+import scIB
+import warnings
+warnings.filterwarnings('ignore')
+import argparse
+
+if __name__=='__main__':
+    """
+    Merge results for cell cycle variance for all scenarios, methods and settings
+    """
+
+    parser = argparse.ArgumentParser(description='Collect all metrics')
+
+    parser.add_argument('-i', '--input', nargs='+', required=True, help='input directory')
+    parser.add_argument('-o', '--output', required=True, help='output file')
+    parser.add_argument('-r', '--root', required=True, 
+                        help='root directory for inferring column names from path')
+    args = parser.parse_args()
+    
+    res_list = []
+    for file in args.input:
+        res = pd.read_csv(file)
+        clean_name = file.replace(args.root, "").replace(".csv", "")
+        res['setting'] = clean_name
+        res_list.append(res)
+    
+    results = pd.concat(res_list)
+    print(results)
+    results.to_csv(args.output, index=False)
