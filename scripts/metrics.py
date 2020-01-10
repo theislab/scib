53a2ace40b0d986a2a6a0fe16fd5dd4b486da077
mbuttner <maren.buettner@helmholtz-muenchen.de>
2020-01-10 11:21:58 +0100

    Typo fix

--- scripts/metrics.py
+++ scripts/metrics.py
@@ -71,7 +71,7 @@ if __name__=='__main__':
         if (adata_int.n_vars < n_hvgs):
             raise ValueError("There are less genes in the uncorrected adata than specified for HVG selection")
     #batch_key might be overwritten, so we match it to the pre-integrated labels
-    if not np.array_equal(adata.obs[batch_key].cat.categories,adata_int.obs[batch_key]):
+    if not np.array_equal(adata.obs[batch_key].cat.categories,adata_int.obs[batch_key].cat.categories):
         test_tab = pd.crosstab(adata.obs[batch_key], adata_int.obs[batch_key])
         #match the name according to the largest crosstab entry in case the batches where reordered 
         match_name = test_tab.index.values[test_tab.values.argmax(0)]
