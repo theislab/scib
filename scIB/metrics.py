a2e3434f577d8d4eb009eacc47feef5005e76802
danielStrobl <daniel.strobl@helmholtz-muenchen.de>
2020-02-11 15:37:24 +0100

    changed to n_vars

--- scIB/metrics.py
+++ scIB/metrics.py
@@ -392,10 +392,10 @@ def hvg_overlap(adata_pre, adata_post, batch, n_hvg=500):
     for i in range(len(adata_pre_list)):#range(len(adata_pre_list)):
         sc.pp.filter_genes(adata_pre_list[i], min_cells=1) # remove genes unexpressed (otherwise hvg might break)
         sc.pp.filter_genes(adata_post_list[i], min_cells=1)
-        n_hvg_tmp = np.minimum(n_hvg, int(0.5*len(adata_pre_list[i].var)))
+        n_hvg_tmp = np.minimum(n_hvg, int(0.5*adata_pre_list[i].n_vars))
         if n_hvg_tmp<n_hvg:
             print(adata_pre_list[i].obs[batch][0]+' has less than the specified number of genes')
-            print('Number of genes: '+str(len(adata_pre_list[i].var)))
+            print('Number of genes: '+str(adata_pre_list[i].n_vars))
         hvg_pre = sc.pp.highly_variable_genes(adata_pre_list[i], flavor='cell_ranger', n_top_genes=n_hvg_tmp, inplace=False)
         tmp_pre = adata_pre_list[i].var.index[hvg_pre['highly_variable']]
         hvg_post = sc.pp.highly_variable_genes(adata_post_list[i], flavor='cell_ranger', n_top_genes=n_hvg_tmp, inplace=False)
