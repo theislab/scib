3f49002b16c0dafb71b685f87998b185332abda1
danielStrobl <daniel.strobl@helmholtz-muenchen.de>
2020-03-05 12:52:50 +0100

    more memory efficient hvg metric now works for every scenario

--- scIB/metrics.py
+++ scIB/metrics.py
@@ -400,9 +400,12 @@ def hvg_overlap(adata_pre, adata_post, batch, n_hvg=500):
     adata_post_list = splitBatches(adata_post, batch)
     overlap = []
     
-    if ('hvg_before' in adata_pre.uns_keys()) and (set(hvg_post) == set(adata_pre.var_names)): # Use precomputed HVGs per batch if available and full feature
+    if ('hvg_before' in adata_pre.uns_keys()) and (set(hvg_post) == set(adata_pre.var_names)):
         print('Using precomputed hvgs per batch')
         hvg_pre_list = adata_pre.uns['hvg_before']
+    
+    else:
+        hvg_pre_list = precompute_hvg_batch(adata_pre, batch, hvg_post)
    
     
         for i in range(len(adata_post_list)):#range(len(adata_pre_list)):
@@ -423,27 +426,7 @@ def hvg_overlap(adata_pre, adata_post, batch, n_hvg=500):
             hvg_post = sc.pp.highly_variable_genes(adata_post_list[i], flavor='cell_ranger', n_top_genes=n_hvg_tmp, inplace=False)
             tmp_post = adata_post_list[i].var.index[hvg_post['highly_variable']]
             n_hvg_real = np.minimum(len(tmp_pre),len(tmp_post))
-            overlap.append((len(set(tmp_pre).intersection(set(tmp_post))))/n_hvg_real)
-    else:
-        adata_pre_list = splitBatches(adata_pre, batch, hvg=hvg_post)
-        for i in range(len(adata_pre_list)):#range(len(adata_pre_list)):
-            sc.pp.filter_genes(adata_pre_list[i], min_cells=1) # remove genes unexpressed (otherwise hvg might break)
-            sc.pp.filter_genes(adata_post_list[i], min_cells=1)
-        
-            ov = list(set(adata_pre_list[i].var_names).intersection(adata_post_list[i].var_names))
-            adata_pre_list[i] = adata_pre_list[i][:,ov]
-            adata_post_list[i] = adata_post_list[i][:,ov]
-        
-            n_hvg_tmp = np.minimum(n_hvg, int(0.5*adata_pre_list[i].n_vars))
-            if n_hvg_tmp<n_hvg:
-                print(adata_pre_list[i].obs[batch][0]+' has less than the specified number of genes')
-                print('Number of genes: '+str(adata_pre_list[i].n_vars))
-            hvg_pre = sc.pp.highly_variable_genes(adata_pre_list[i], flavor='cell_ranger', n_top_genes=n_hvg_tmp, inplace=False)
-            tmp_pre = adata_pre_list[i].var.index[hvg_pre['highly_variable']]
-            hvg_post = sc.pp.highly_variable_genes(adata_post_list[i], flavor='cell_ranger', n_top_genes=n_hvg_tmp, inplace=False)
-            tmp_post = adata_post_list[i].var.index[hvg_post['highly_variable']]
-            n_hvg_real = np.minimum(len(tmp_pre),len(tmp_post))
-            overlap.append((len(set(tmp_pre).intersection(set(tmp_post))))/n_hvg_real)
+            overlap.append((len(set(tmp_pre).intersection(set(tmp_post))))/n_hvg_real) 
     return np.mean(overlap)
 
 
