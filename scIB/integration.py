6724287180ad777702185599e59c5171adc7f700
LuckyMD <malte.luecken@helmholtz-muenchen.de>
2020-03-15 13:51:15 +0100

    changed bbknn to defaults

--- scIB/integration.py
+++ scIB/integration.py
@@ -278,7 +278,7 @@ def runBBKNN(adata, batch, hvg=None):
     checkSanity(adata, batch, hvg)
     sc.pp.pca(adata, svd_solver='arpack')
     #if adata.n_obs <1e5:
-    corrected = bbknn.bbknn(adata, batch_key=batch,trim=10, neighbors_within_batch=5, copy=True)
+    corrected = bbknn.bbknn(adata, batch_key=batch, copy=True)
     #if adata.n_obs >=1e5:
     #    corrected = bbknn.bbknn(adata, batch_key=batch,trim=30, neighbors_within_batch=30, copy=True)
     return corrected
