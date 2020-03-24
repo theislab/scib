8ec468668c08306841606e2ff5c2b9353c4e4cda
Maren Büttner <maren.buettner@helmholtz-muenchen.de>
2020-03-24 09:11:03 +0100

    remove LISI call and replace by lisi_graph

--- scIB/metrics.py
+++ scIB/metrics.py
@@ -1571,7 +1571,7 @@ def metrics(adata, adata_int, batch_key, label_key,
             silhouette_=False,  embed='X_pca', si_metric='euclidean',
             pcr_=False, cell_cycle_=False, organism='mouse', verbose=False,
             isolated_labels_=False, n_isolated=None,
-            kBET_=False, kBET_sub=0.5, lisi_=False, lisi_graph_=False,
+            kBET_=False, kBET_sub=0.5, lisi_graph_=False,
             trajectory_= False, type_ = None
            ):
     """
@@ -1668,15 +1668,15 @@ def metrics(adata, adata_int, batch_key, label_key,
         kbet_score = np.nan
     results['kBET'] = kbet_score
 
-    if lisi_:
-        print('LISI score...')
-        ilisi_score, clisi_score = lisi(adata_int, batch_key=batch_key, label_key=label_key,
-                                        type_ = type_, verbose=verbose)
-    else:
-        ilisi_score = np.nan
-        clisi_score = np.nan
-    results['iLISI'] = ilisi_score
-    results['cLISI'] = clisi_score
+    #if lisi_:
+    #    print('LISI score...')
+    #    ilisi_score, clisi_score = lisi(adata_int, batch_key=batch_key, label_key=label_key,
+    #                                    type_ = type_, verbose=verbose)
+    #else:
+    #    ilisi_score = np.nan
+    #    clisi_score = np.nan
+    #results['iLISI'] = ilisi_score
+    #results['cLISI'] = clisi_score
     
     if lisi_graph_:
         print('LISI graph score...')
@@ -1686,8 +1686,8 @@ def metrics(adata, adata_int, batch_key, label_key,
     else:
         ilisi_g_score = np.nan
         clisi_g_score = np.nan
-    results['iLISI_graph'] = ilisi_g_score
-    results['cLISI_graph'] = clisi_g_score
+    results['iLISI'] = ilisi_g_score
+    results['cLISI'] = clisi_g_score
         
     if hvg_score_:
         hvg_score = hvg_overlap(adata, adata_int, batch_key)
