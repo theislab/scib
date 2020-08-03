883ef0eeb95df4ad0b43a97db3fc20f449ad3542
LuckyMD <malte.luecken@helmholtz-muenchen.de>
2020-08-03 17:38:10 +0200

    added docstring with origin of parametrization

--- scIB/integration.py
+++ scIB/integration.py
@@ -330,6 +330,12 @@ def runCombat(adata, batch):
 
 
 def runDESC(adata, batch, res=0.8, ncores=24, tmp_dir='/localscratch/'):
+    """
+    Convenience function to run DESC. Parametrization was taken from:
+    https://github.com/eleozzr/desc/issues/28
+
+    as suggested by the developer (rather than from the tutorial notebook).
+    """
     import desc
 
     adata_out = adata.copy()
