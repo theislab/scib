870a6f2cfc6dd45e825bf52f92a0cca60761ee2e
Luke Zappia <lazappi@users.noreply.github.com>
2020-09-21 16:55:18 +0200

    Add UMAP output to metrics

--- scripts/metrics.py
+++ scripts/metrics.py
@@ -19,28 +19,28 @@ if __name__=='__main__':
     """
     read adata object, compute all metrics and output csv.
     """
-    
+
     import argparse
     import os
 
     parser = argparse.ArgumentParser(description='Compute all metrics')
-    
+
     parser.add_argument('-u', '--uncorrected', required=True)
     parser.add_argument('-i', '--integrated', required=True)
     parser.add_argument('-o', '--output', required=True, help='Output file')
     parser.add_argument('-m', '--method', required=True, help='Name of method')
-    
+
     parser.add_argument('-b', '--batch_key', required=True, help='Key of batch')
     parser.add_argument('-l', '--label_key', required=True, help='Key of annotated labels e.g. "cell_type"')
-    
+
     parser.add_argument('--organism', required=True)
     parser.add_argument('--type', required=True, choices=RESULT_TYPES, help='Type of result: full, embed, knn\n full: scanorama, seurat, MNN\n embed: scanorama, Harmony\n knn: BBKNN')
     parser.add_argument('--assay', default='expression', choices=ASSAYS, help='Experimental assay')
     parser.add_argument('--hvgs', default=0, help='Number of highly variable genes. Use 0 to specify that no feature selection had been used.', type=int)
     parser.add_argument('-v', '--verbose', action='store_true')
-    
+
     args = parser.parse_args()
-    
+
     verbose = args.verbose
     type_ = args.type
     batch_key = args.batch_key
@@ -48,10 +48,10 @@ if __name__=='__main__':
     assay = args.assay
     organism = args.organism
     n_hvgs = args.hvgs if args.hvgs > 0 else None
-    
+
     # encode setup for column name
     setup = f'{args.method}_{args.type}'
-    
+
     # create cluster NMI output file
     file_stump = os.path.splitext(args.output)[0]
     cluster_nmi = f'{file_stump}_nmi.txt'
@@ -66,7 +66,7 @@ if __name__=='__main__':
         print(f'    n_hvgs:\t{n_hvgs}')
         print(f'    setup:\t{setup}')
         print(f'    optimised clustering results:\t{cluster_nmi}')
-    
+
     ###
 
 
@@ -87,13 +87,13 @@ if __name__=='__main__':
     if (n_hvgs is not None):
         if (adata_int.n_vars < n_hvgs):
             raise ValueError("There are less genes in the corrected adata than specified for HVG selection")
-       
+
     # check input files
     if adata.n_obs != adata_int.n_obs:
         message = "The datasets have different numbers of cells before and after integration."
         message += "Please make sure that both datasets match."
         raise ValueError(message)
-    
+
     #check if the obsnames were changed and rename them in that case
     if len(set(adata.obs_names).difference(set(adata_int.obs_names))) > 0:
         #rename adata_int.obs[batch_key] labels by overwriting them with the pre-integration labels
@@ -103,11 +103,11 @@ if __name__=='__main__':
             adata_int.obs_names = new_obs_names
         else:
             raise ValueError('obs_names changed after integration!')
-            
+
     #batch_key might be overwritten, so we match it to the pre-integrated labels
     adata_int.obs[batch_key] = adata_int.obs[batch_key].astype('category')
     if not np.array_equal(adata.obs[batch_key].cat.categories,adata_int.obs[batch_key].cat.categories):
-        #pandas uses the table index to match the correct labels 
+        #pandas uses the table index to match the correct labels
         adata_int.obs[batch_key] = adata.obs[batch_key]
         #print(adata.obs[batch_key].value_counts())
         #print(adata_int.obs[batch_key].value_counts())
@@ -118,20 +118,20 @@ if __name__=='__main__':
         message = "There are fewer genes in the uncorrected adata "
         message += "than specified for HVG selection."
         raise ValueError(message)
-    
+
     # DATA REDUCTION
     # select options according to type
-    
+
     # case 1: full expression matrix, default settings
     precompute_pca = True
     recompute_neighbors = True
     embed = 'X_pca'
-    
+
     # distinguish between subsetted and full expression matrix
     # compute HVGs only if output is not already subsetted
     if adata.n_vars > adata_int.n_vars:
         n_hvgs = None
-    
+
     # case 2: embedding output
     if (type_ == "embed"):
         n_hvgs = None
@@ -139,13 +139,13 @@ if __name__=='__main__':
         # legacy check
         if ('emb' in adata_int.uns) and (adata_int.uns['emb']):
             adata_int.obsm["X_emb"] = adata_int.obsm["X_pca"].copy()
-    
+
     # case3: kNN graph output
     elif (type_ == "knn"):
         n_hvgs = None
         precompute_pca = False
         recompute_neighbors = False
-    
+
     if verbose:
         print('reduce integrated data:')
         print(f'    HVG selection:\t{n_hvgs}')
@@ -160,7 +160,7 @@ if __name__=='__main__':
                                        n_top_genes=n_hvgs,
                                        neighbors=recompute_neighbors, use_rep=embed,
                                        pca=precompute_pca, umap=False)
-    
+
     print("computing metrics")
     # DEFAULT
     silhouette_ = True
@@ -174,8 +174,8 @@ if __name__=='__main__':
     kBET_ = True
     #lisi_ = True
     lisi_graph_ = True
-    
-    
+
+
     # by output type
     if (type_ == "embed"):
         hvg_score_ = False
@@ -185,14 +185,14 @@ if __name__=='__main__':
         cell_cycle_ = False
         hvg_score_ = False
         #lisi_ = False
-    
+
      # by assay
     if args.assay == 'atac':
         cell_cycle_ = False
         hvg_score_ = False
     elif args.assay == 'simulation':
         cell_cycle_ = False
-    
+
     # check if pseudotime data exists in original data
     if 'dpt_pseudotime' in adata.obs:
         trajectory_ = True
@@ -230,12 +230,12 @@ if __name__=='__main__':
         #print(f'    LISI:\t{lisi_}')
         print(f'    LISI:\t{lisi_graph_}')
         print(f'    Trajectory:\t{trajectory_}')
-        
+
     results = scIB.me.metrics(adata, adata_int, verbose=verbose,
                               hvg_score_=hvg_score_, cluster_nmi=cluster_nmi,
                               batch_key=batch_key, label_key=label_key,
                               silhouette_=silhouette_, embed=embed,
-                              type_ = type_, 
+                              type_ = type_,
                               nmi_=nmi_, nmi_method='arithmetic', nmi_dir=None,
                               ari_=ari_,
                               pcr_=pcr_,
@@ -253,5 +253,18 @@ if __name__=='__main__':
     # save metrics' results
     results.to_csv(args.output)
 
-    print("done")
+    outdir = os.path.dirname(args.output)
+    print(f'Calculating UMAP...')
+    sc.tl.umap(adata_int)
+    print(f'Saving UMAP for labels "{label_key}"...')
+    fig = sc.pl.umap(adata_int, color=label_key, return_fig=True)
+    fig.set_size_inches(10, 8)
+    fig.savefig(os.path.join(outdir, f'{args.method}_{args.type}_labels.png'),
+                bbox_inches='tight')
+    print(f'Saving UMAP for batches "{batch_key}"...')
+    fig = sc.pl.umap(adata_int, color=batch_key, return_fig=True)
+    fig.set_size_inches(10, 8)
+    fig.savefig(os.path.join(outdir, f'{args.method}_{args.type}_batch.png'),
+                bbox_inches='tight')
 
+    print("done")
