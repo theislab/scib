10b83fe14a0a53495be4d71686a5a6f4184c67d6
martaint <31702330+martaint@users.noreply.github.com>
2021-02-16 11:18:39 +0100

    updated func doc

--- R/visualization/plotBestMethodsRNA.R
+++ R/visualization/plotBestMethodsRNA.R
@@ -1,8 +1,9 @@
-#' Plotting scib-metrics result of the integration across multiple RNA tasks in an
+#' Plot scib best methods - RNA
+#' @description Plotting scib-metrics result of the integration across multiple RNA tasks in an
 #'  overview plot that shows the best-performing combination of pre-processing choices for 
 #'  each integration method.
 #'
-#' \code{plotBestMethodsRNA} saves in `outdir` the overview table in three formats (.pdf/.tiff/.png)
+#' @return \code{plotBestMethodsRNA} saves in `outdir` the overview table in three formats (.pdf/.tiff/.png)
 #'
 #' @param csv_metrics_path path to a .csv file output of scib pipeline that contains the metrics calculated 
 #' across multiple tasks. All tasks will be considered for ranking best methods, except simulations.
