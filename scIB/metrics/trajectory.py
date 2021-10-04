8d56e801f95e11980eef2f6ffc0544cfed25dea0
Strobl <daniel.strobl@mb084184.dyn.scidom.de>
2021-10-04 14:18:36 +0200

    add per batch trajectory score

--- scIB/metrics/trajectory.py
+++ scIB/metrics/trajectory.py
@@ -68,6 +68,7 @@ def trajectory_conservation(
         adata_post,
         label_key,
         pseudotime_key="dpt_pseudotime"
+	batch_key=False
 ):
     """
     :param adata_pre: unintegrated adata
@@ -75,29 +76,52 @@ def trajectory_conservation(
     :param label_key: column in `adata_pre.obs` of the groups used to precompute the trajectory
     :param pseudotime_key: column in `adata_pre.obs` in which the pseudotime is saved in.
         Column can contain empty entries, the dataset will be subset to the cells with scores.
+    :param batch_key: set to batch key if if you want to compute the trajectory metric by batch
     """
     # subset to cells for which pseudotime has been computed
     cell_subset = adata_pre.obs.index[adata_pre.obs[pseudotime_key].notnull()]
     adata_pre_sub = adata_pre[cell_subset]
     adata_post_sub = adata_post[cell_subset]
+    if batch_key==False:
+        try:
+            iroot, adata_post_sub2 = get_root(adata_pre_sub, adata_post_sub, label_key, pseudotime_key)
+        except RootCellError:
+            print('No root cell found, setting trajectory conservation metric to 0.')
+            return 0  # failure to find root cell means no TI conservation
 
-    try:
-        iroot, adata_post_sub2 = get_root(adata_pre_sub, adata_post_sub, label_key, pseudotime_key)
-    except RootCellError:
-        print('No root cell found, setting trajectory conservation metric to 0.')
-        return 0  # failure to find root cell means no TI conservation
+        adata_post_sub2.uns['iroot'] = iroot
 
-    adata_post_sub2.uns['iroot'] = iroot
+        sc.tl.dpt(adata_post_sub2)  # stored in 'dpt_pseudotime'
+        adata_post_sub2.obs.loc[adata_post_sub2.obs['dpt_pseudotime'] > 1, 'dpt_pseudotime'] = 0
+        adata_post_sub.obs['dpt_pseudotime'] = 0
+        adata_post_sub.obs['dpt_pseudotime'] = adata_post_sub2.obs['dpt_pseudotime']
+        adata_post_sub.obs['dpt_pseudotime'].fillna(0, inplace=True)
 
-    sc.tl.dpt(adata_post_sub2)  # stored in 'dpt_pseudotime'
-    adata_post_sub2.obs.loc[adata_post_sub2.obs['dpt_pseudotime'] > 1, 'dpt_pseudotime'] = 0
-    adata_post_sub.obs['dpt_pseudotime'] = 0
-    adata_post_sub.obs['dpt_pseudotime'] = adata_post_sub2.obs['dpt_pseudotime']
-    adata_post_sub.obs['dpt_pseudotime'].fillna(0, inplace=True)
+        pseudotime_before = adata_post_sub.obs['dpt_pseudotime']
+        pseudotime_after = adata_pre_sub.obs[pseudotime_key]
+        correlation = pseudotime_before.corr(pseudotime_after, 'spearman')
 
-    pseudotime_before = adata_post_sub.obs['dpt_pseudotime']
-    pseudotime_after = adata_pre_sub.obs[pseudotime_key]
-    correlation = pseudotime_before.corr(pseudotime_after, 'spearman')
-
-    return (correlation + 1) / 2  # scaled
-
+        return (correlation + 1) / 2  # scaled
+    else:
+        corr = pd.Series()
+        for i in batch_key:
+            adata_pre_sub_tmp = adata_pre_sub[adata_pre_sub.obs[batch_key]==i]
+            adata_post_sub_tmp = adata_post_sub[adata_post_sub.obs[batch_key]==i]
+            try:
+                iroot, adata_post_sub2_tmp = get_root(adata_pre_sub_tmp, adata_post_sub_tmp, label_key, pseudotime_key)
+            except RootCellError:
+                print('No root cell found, setting trajectory conservation metric to 0.')
+                return 0  # failure to find root cell means no TI conservation
+            
+            adata_post_sub2_tmp.uns['iroot'] = iroot
+            
+            sc.tl.dpt(adata_post_sub2_tmp)  # stored in 'dpt_pseudotime'
+            adata_post_sub2_tmp.obs.loc[adata_post_sub2_tmp.obs['dpt_pseudotime'] > 1, 'dpt_pseudotime'] = 0
+            adata_post_sub_tmp.obs['dpt_pseudotime'] = 0
+            adata_post_sub_tmp.obs['dpt_pseudotime'] = adata_post_sub2_tmp.obs['dpt_pseudotime']
+            adata_post_sub_tmp.obs['dpt_pseudotime'].fillna(0, inplace=True)
+            
+            pseudotime_before = adata_post_sub_tmp.obs['dpt_pseudotime']
+            pseudotime_after = adata_pre_sub_tmp.obs[pseudotime_key]
+            corr[i] = pseudotime_before.corr(pseudotime_after, 'spearman')
+        return (corr.mean() + 1) / 2 # scaled
