631c950cf631dcd900beb194d00674e7bbd03978
Michaela Mueller <mumichae@in.tum.de>
2021-10-22 13:03:25 +0200

    Revert "Throw error when batches mismatch in trajectory conservation metric"
    This reverts commit fe0200cf8e7f5578a752c8170d23a65b7c821215.

--- scib/metrics/trajectory.py
+++ scib/metrics/trajectory.py
@@ -98,12 +98,7 @@ def trajectory_conservation(
     adata_post_ti.obs['dpt_pseudotime'] = adata_post_ti2.obs['dpt_pseudotime']
     adata_post_ti.obs['dpt_pseudotime'].fillna(0, inplace=True)
 
-    if adata_post_ti.obs['batch'] != adata_pre_ti.obs['batch']:
-        raise ValueError(
-            'Batch columns do not match\n'
-            f"adata_post_ti.obs['batch']:\n {adata_post_ti.obs['batch']}\n"
-            f"adata_pre_ti.obs['batch']:\n {adata_pre_ti.obs['batch']}\n"
-        )
+    adata_post_ti.obs['batch'] = adata_pre_ti.obs['batch']
 
     if batch_key == None:
         pseudotime_before = adata_pre_ti.obs[pseudotime_key]
