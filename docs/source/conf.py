f9b584ce5f59787f458dc4b4dd6ddc0a5508f1a4
Michaela Mueller <mumichae@in.tum.de>
2021-12-01 21:49:13 +0100

    remove myst_parser dependency

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -36,7 +36,6 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
-    'myst_parser',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
