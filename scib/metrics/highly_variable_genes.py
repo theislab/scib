f55d011f046093d4a70066ffc9380173e14bed2c
Michaela Mueller <michaela.mueller@helmholtz-muenchen.de>
2022-03-21 11:34:19 +0100

    reformatted for more readability

--- scib/metrics/highly_variable_genes.py
+++ scib/metrics/highly_variable_genes.py
@@ -52,18 +52,23 @@ def hvg_overlap(adata_pre, adata_post, batch, n_hvg=500, verbose=False):
 
     hvg_pre_list = precompute_hvg_batch(adata_pre, batch, hvg_post)
 
-    for i in range(len(adata_post_list)):  # range(len(adata_pre_list)):
-        sc.pp.filter_genes(adata_post_list[i], min_cells=1)  # remove genes unexpressed (otherwise hvg might break)
-        batch_var = adata_post_list[i].obs[batch][0]
+    for ad_post in adata_post_list:  # range(len(adata_pre_list)):
+        # remove genes unexpressed (otherwise hvg might break)
+        sc.pp.filter_genes(ad_post, min_cells=1)
+        batch_var = ad_post.obs[batch][0]
         n_hvg_tmp = len(hvg_pre_list[batch_var])
-        
+
         if verbose:
             print(n_hvg_tmp)
-            
+
         tmp_pre = hvg_pre_list[batch_var]
-        hvg_post = sc.pp.highly_variable_genes(adata_post_list[i], flavor='cell_ranger', n_top_genes=n_hvg_tmp,
-                                               inplace=False)
-        tmp_post = adata_post_list[i].var.index[hvg_post['highly_variable']]
+        hvg_post = sc.pp.highly_variable_genes(
+            ad_post,
+            flavor='cell_ranger',
+            n_top_genes=n_hvg_tmp,
+            inplace=False
+        )
+        tmp_post = ad_post.var.index[hvg_post['highly_variable']]
         n_hvg_real = np.minimum(len(tmp_pre), len(tmp_post))
         overlap.append((len(set(tmp_pre).intersection(set(tmp_post)))) / n_hvg_real)
     return np.mean(overlap)
