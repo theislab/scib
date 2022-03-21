bb1c5c37ed18cce232166cb832255f59a909da62
Michaela Mueller <michaela.mueller@helmholtz-muenchen.de>
2022-03-21 16:10:23 +0100

    fixed module scope

--- scib/metrics/__init__.py
+++ scib/metrics/__init__.py
@@ -1,16 +1,2 @@
-__all__ = [
-    "ari",
-    "cell_cycle",
-    "graph_connectivity",
-    "highly_variable_genes",
-    "isolated_labels",
-    "kbet",
-    "lisi",
-    "metrics",
-    "nmi",
-    "pcr",
-    "silhouette",
-    "trajectory"
-]
-
 from .metrics import *
+from .lisi import lisi_graph
