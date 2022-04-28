b4f2794890fac1a8f4cb06c02763ea776b15c1b2
Michaela Mueller <michaela.mueller@helmholtz-muenchen.de>
2022-04-28 19:13:51 +0200

    add check for g++

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 import os
+import shutil
 
 from setuptools import setup
 
-os.system('g++ -std=c++11 -O3 scib/knn_graph/knn_graph.cpp -o scib/knn_graph/knn_graph.o')
+if shutil.which('g++') is not None:
+    os.system('g++ -std=c++11 -O3 scib/knn_graph/knn_graph.cpp -o scib/knn_graph/knn_graph.o')
 setup()
