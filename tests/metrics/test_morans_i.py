bcb085aa6a76d33512264368137b8b59f339f6bb
Leon Hetzel <leon.het@googlemail.com>
2022-05-03 14:36:55 +0200

    Add test for compare_pre, adjust expected scores

--- tests/metrics/test_morans_i.py
+++ tests/metrics/test_morans_i.py
@@ -10,7 +10,7 @@ def test_morans_i_nhvg(adata_neighbors):
         n_hvg=100,
     )
     LOGGER.info(f"score: {score}")
-    expected_score = 0.65204
+    expected_score = 0.30409
     assert_near_exact(score, expected_score, diff=1e-5)
 
 
@@ -34,5 +34,19 @@ def test_morans_i_hvgs(adata_neighbors):
         ],
     )
     LOGGER.info(f"score: {score}")
-    expected_score = 0.64337
+    expected_score = 0.28675
+    assert_near_exact(score, expected_score, diff=1e-5)
+
+
+def test_morans_i_compare_pre(adata_neighbors):
+    adata_int = adata_neighbors.copy()
+    score = scib.me.morans_i(
+        adata_pre=adata_neighbors,
+        adata_post=adata_int,
+        batch_key="batch",
+        n_hvg=100,
+        compare_pre=True,
+    )
+    LOGGER.info(f"score: {score}")
+    expected_score = 0.94336
     assert_near_exact(score, expected_score, diff=1e-5)
