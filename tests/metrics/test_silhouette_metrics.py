612ff188c3faf141fc39baafbffe3b0d1e923307
Michaela Mueller <51025211+mumichae@users.noreply.github.com>
2022-10-16 11:51:49 +0200

    Fix empty dataframe bug for silhouette batch score (#342)

--- tests/metrics/test_silhouette_metrics.py
+++ tests/metrics/test_silhouette_metrics.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 import scib
 from tests.common import LOGGER, assert_near_exact
 
@@ -21,6 +23,24 @@ def test_silhouette_batch(adata_pca):
     )
     LOGGER.info(f"score: {score}")
     assert_near_exact(score, 0.9014384369842835, diff=1e-2)
+
+
+def test_silhouette_batch_empty(adata_pca):
+    adata_pca.obs["label"] = "label"
+    adata_pca.obs["batch"] = "batch"
+    asw, sil_means, sil_df = scib.me.silhouette_batch(
+        adata_pca,
+        batch_key="batch",
+        group_key="celltype",
+        embed="X_pca",
+        scale=True,
+        verbose=False,
+        return_all=True,
+    )
+    assert np.isnan(asw)
+    assert np.isnan(sil_means)
+    assert sil_df.shape[0] == 0
+    LOGGER.info(sil_df)
 
 
 def test_isolated_labels_silhouette(adata_pca):
