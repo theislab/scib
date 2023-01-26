1be0daa10399af8619937c4825eea64805d90d79
Daniel Strobl <danielstrobl@gmail.com>
2023-01-26 20:00:21 +0100

    Revert "Update isolated_labels.py"
    This reverts commit 972ad6c2dd32eb31de3a4f1d5ccac8fa23c316bd.

--- scib/metrics/isolated_labels.py
+++ scib/metrics/isolated_labels.py
@@ -1,6 +1,5 @@
 import pandas as pd
 from sklearn.metrics import f1_score
-from sklearn.metrics import silhouette_samples
 
 from .clustering import cluster_optimal_resolution
 from .silhouette import silhouette
@@ -228,9 +227,7 @@ def score_isolated_label(
     else:
         # AWS score between isolated label vs rest
         adata.obs[iso_label_key] = adata.obs[label_key] == isolated_label
-        adata.obs['silhouette_temp'] = silhouette_samples(adata.obsm[embed], adata.obs[iso_label_key])
-        
-        score = adata.obs[adata.obs[iso_label_key]].silhouette_temp.mean()
+        score = silhouette(adata, iso_label_key, embed)
 
     if verbose:
         print(f"{isolated_label}: {score}")
