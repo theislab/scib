baffd8ad01ce6db8f71e03c50b987f319f227f22
Michaela Mueller <michaela.mueller@helmholtz-muenchen.de>
2024-04-17 14:44:22 +0200

    add tests for precomputed clusters

--- tests/preprocessing/test_clustering.py
+++ tests/preprocessing/test_clustering.py
@@ -31,6 +31,7 @@ def test_cluster_optimal_resolution_louvain(adata_neighbors):
         return_all=True,
     )
     assert isinstance(score_all, pd.DataFrame)
+    assert "cluster" in adata_neighbors.obs.columns
 
     LOGGER.info(f"max resolution: {res_max}, max score: {score_max}")
     assert res_max == 0.7
@@ -46,7 +47,40 @@ def test_cluster_optimal_resolution_leiden(adata_neighbors):
         return_all=True,
     )
     assert isinstance(score_all, pd.DataFrame)
+    assert "cluster" in adata_neighbors.obs.columns
 
     LOGGER.info(f"max resolution: {res_max}, max score: {score_max}")
     assert res_max == 0.5
     assert_near_exact(score_max, 0.7424614219722735, diff=1e-3)
+
+
+def test_precomputed_cluster(adata):
+    resolutions = scib.cl.get_resolutions(n=5, min=0.1, max=2)
+    for res in resolutions:
+        adata.obs[f"cluster_{res}"] = adata.obs["celltype"]
+
+    res_max, score_max = scib.cl.cluster_optimal_resolution(
+        adata,
+        cluster_key="cluster",
+        label_key="celltype",
+        force=False,
+        resolutions=resolutions,
+    )
+    assert res_max == 0.1
+    assert_near_exact(score_max, 1, diff=0)
+
+
+def test_precomputed_cluster_force(adata_neighbors):
+    resolutions = scib.cl.get_resolutions(n=5, min=0.1, max=2)
+    for res in resolutions:
+        adata_neighbors.obs[f"cluster_{res}"] = adata_neighbors.obs["celltype"]
+
+    res_max, score_max = scib.cl.cluster_optimal_resolution(
+        adata_neighbors,
+        cluster_key="cluster",
+        label_key="celltype",
+        resolutions=resolutions,
+        force=True,
+    )
+    assert res_max == 0.5
+    assert_near_exact(score_max, 0.7424614219722736, diff=1e-5)
