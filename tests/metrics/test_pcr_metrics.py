8c7df11e81614a3c64b28ae2224e4197a0b84966
Michaela Mueller <michaela.mueller@helmholtz-muenchen.de>
2024-04-22 17:12:40 +0200

    add tests for different linreg implementations

--- tests/metrics/test_pcr_metrics.py
+++ tests/metrics/test_pcr_metrics.py
@@ -6,19 +6,55 @@ from tests.common import LOGGER, add_embed, assert_near_exact
 
 def test_pc_regression(adata):
     score = scib.me.pc_regression(adata.X, adata.obs["batch"])
-    LOGGER.info(f"using embedding: {score}")
+    LOGGER.info(score)
     assert_near_exact(score, 0, diff=1e-4)
 
 
 def test_pc_regression_sparse(adata):
-    x = csr_matrix(adata.X)
-    score = scib.me.pc_regression(x, adata.obs["batch"], n_comps=x.shape[1])
-    LOGGER.info(f"using embedding: {score}")
+    score = scib.me.pc_regression(
+        csr_matrix(adata.X),
+        covariate=adata.obs["batch"],
+        n_comps=adata.n_vars,
+    )
+    LOGGER.info(score)
     assert_near_exact(score, 0, diff=1e-4)
 
 
-def test_pcr_batch(adata):
-    # no PCA precomputed
+def test_pcr_sklearn(adata_pca):
+    score = scib.me.pcr(
+        adata_pca, covariate="celltype", linreg_method="sklearn", verbose=True
+    )
+    LOGGER.info(score)
+    assert_near_exact(score, 0.3371261556141021, diff=1e-3)
+
+
+def test_pcr_numpy(adata_pca):
+    score = scib.me.pcr(
+        adata_pca, covariate="celltype", linreg_method="numpy", verbose=True
+    )
+    LOGGER.info(score)
+    assert_near_exact(score, 0.3371261556141021, diff=1e-3)
+
+
+def test_pcr_implementations(adata_pca):
+    score_sklearn = scib.me.pcr(
+        adata_pca,
+        covariate="celltype",
+        linreg_method="sklearn",
+    )
+    LOGGER.info(f"sklearn score: {score_sklearn}")
+
+    score_numpy = scib.me.pcr(
+        adata_pca,
+        covariate="celltype",
+        linreg_method="numpy",
+    )
+    LOGGER.info(f"numpy score: {score_numpy}")
+
+    assert_near_exact(score_sklearn, score_numpy, diff=1e-3)
+
+
+def test_pcr_comparison_batch(adata):
     score = scib.me.pcr_comparison(
         adata, adata, covariate="batch", n_comps=50, scale=True
     )
@@ -26,13 +62,13 @@ def test_pcr_batch(adata):
     assert_near_exact(score, 0, diff=1e-6)
 
 
-def test_pcr_batch_precomputed(adata_pca):
+def test_pcr_comparison_batch_precomputed(adata_pca):
     score = scib.me.pcr_comparison(adata_pca, adata_pca, covariate="batch", scale=True)
     LOGGER.info(f"precomputed PCA: {score}")
     assert_near_exact(score, 0, diff=1e-6)
 
 
-def test_pcr_batch_embedding(adata):
+def test_pcr_comparison_batch_embedding(adata):
     # use different embedding
     score = scib.me.pcr_comparison(
         adata_pre=adata,
@@ -42,5 +78,5 @@ def test_pcr_batch_embedding(adata):
         n_comps=50,
         scale=True,
     )
-    LOGGER.info(f"using embedding: {score}")
+    LOGGER.info(f"using X_emb: {score}")
     assert_near_exact(score, 0, diff=1e-6)
