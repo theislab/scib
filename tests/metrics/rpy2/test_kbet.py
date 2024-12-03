017c5dc4726de5e71138eab2cfc1b4c2ca419fcf
Michaela Mueller <michaela.mueller@helmholtz-muenchen.de>
2024-12-03 22:17:12 +0100

    adjust margin and add random batch kbet score test

--- tests/metrics/rpy2/test_kbet.py
+++ tests/metrics/rpy2/test_kbet.py
@@ -1,3 +1,7 @@
+import numpy as np
+import scanpy as sc
+from tqdm import tqdm
+
 import scib
 from tests.common import LOGGER, assert_near_exact
 
@@ -7,4 +11,21 @@ def test_kbet(adata_pca):
         adata_pca, batch_key="batch", label_key="celltype", embed="X_pca", type_="full"
     )
     LOGGER.info(f"score: {score}")
-    assert_near_exact(score, 0.556108994805538, diff=1e-02)
+    assert_near_exact(score, 0.55, diff=1e-01)
+
+
+def test_kbet_random(adata_pca):
+    scores = []
+    for _ in tqdm(range(10)):
+        adata_pca.obs["batch"] = adata_pca.obs["batch"].sample(frac=1).values
+        sc.pp.pca(adata_pca, n_comps=20, use_highly_variable=True)
+        score = scib.me.kBET(
+            adata_pca,
+            batch_key="batch",
+            label_key="celltype",
+            embed="X_pca",
+            type_="full",
+        )
+        LOGGER.info(f"score: {score}")
+        scores.append(score)
+    assert_near_exact(np.mean(scores), 0.8, diff=1e-01)
