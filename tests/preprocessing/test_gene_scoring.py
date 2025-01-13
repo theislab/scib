92b9ebf3ea4db128674c5e4889fd3815dbac073c
Michaela Mueller <michaela.mueller@helmholtz-muenchen.de>
2025-01-13 18:50:58 +0100

    add assertions for gene score columns

--- tests/preprocessing/test_gene_scoring.py
+++ tests/preprocessing/test_gene_scoring.py
@@ -5,10 +5,19 @@ import scib
 
 
 def test_mouse(adata_paul15):
+
+    assert "S_score" not in adata_paul15.obs.columns
+    assert "G2M_score" not in adata_paul15.obs.columns
+    assert "phase" not in adata_paul15.obs.columns
+
     scib.pp.score_cell_cycle(
         adata_paul15,
         organism="mouse",
     )
+    assert "S_score" in adata_paul15.obs.columns
+    assert "G2M_score" in adata_paul15.obs.columns
+    assert "phase" in adata_paul15.obs.columns
+
     scib.pp.score_cell_cycle(
         adata_paul15,
         organism="mus musculus",
